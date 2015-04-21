502f703ecc1dd210804c48d6afe067c9b9749d41
kimtw <kimtw@kimtw-PC>
2015-04-21 12:31:57 +0900

    test

---
+++ src/com/darkgoon83/bbsController.java
@@ -0,0 +1,17 @@
+package com.darkgoon83;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller 
+public class bbsController {
+	
+	@RequestMapping("/bbs")
+	
+	public ModelAndView bbs() {
+		String sMessage = "스프링 프레임워크 테스트";
+		return new ModelAndView("bbs", "message", sMessage);
+	}
+
+}
