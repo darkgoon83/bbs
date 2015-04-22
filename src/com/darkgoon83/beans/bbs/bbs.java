05d1d8fccbcbd4bbad1405173ca90499ff6c617d
kimtw <kimtw@kimtw-PC>
2015-04-22 17:22:20 +0900

    수정완료

---
+++ src/com/darkgoon83/beans/bbs/bbs.java
@@ -0,0 +1,94 @@
+package com.darkgoon83.beans.bbs;
+
+public class bbs {
+	private String idx;
+	private String title;
+	private String contents;
+	private String email;
+	private String phone;
+	private String password;
+	private int view_cnt;
+	private String writer_name;
+	private String writer_id;
+	private String reg_date;
+	private String mode_date;
+	private String writer_ip;
+	
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getView_cnt() {
+		return view_cnt;
+	}
+	public void setView_cnt(int view_cnt) {
+		this.view_cnt = view_cnt;
+	}
+	public String getWriter_name() {
+		return writer_name;
+	}
+	public void setWriter_name(String writer_name) {
+		this.writer_name = writer_name;
+	}
+	public String getWriter_id() {
+		return writer_id;
+	}
+	public void setWriter_id(String writer_id) {
+		this.writer_id = writer_id;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getMode_date() {
+		return mode_date;
+	}
+	public void setMode_date(String mode_date) {
+		this.mode_date = mode_date;
+	}
+	public String getWriter_ip() {
+		return writer_ip;
+	}
+	public void setWriter_ip(String writer_ip) {
+		this.writer_ip = writer_ip;
+	}
+	
+	
+	
+	
+	
+}
